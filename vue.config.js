7fa41a9baf70fc0221e54893447e6b3854061031
Sujil Maharjan <smaharj1@ramapo.edu>
2018-11-29 01:23:03 -0500

    Initial project structure setup done

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
+/**
+ * This is an extension for the webpack that resides in the cli-service Plugin.
+ * This extension configures the prerender-spa-plugin
+ * @author Sujil Maharjan
+*/
+/* eslint-disable */
+module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV !== 'production') return;
+
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          // Absolute path to compiled SPA
+          staticDir: path.resolve(__dirname, 'dist'),
+          // List of routes to prerender
+          routes: ['/', '/about'],
+        }),
+      ],
+    };
+  },
+};
