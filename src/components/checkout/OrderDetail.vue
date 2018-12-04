2e97afd680ff1e49173193b936f7c88e20d59d82
Sujil Maharjan <smaharj1@ramapo.edu>
2018-12-04 11:53:09 -0500

    Added order detail

--- src/components/checkout/OrderDetail.vue
+++ src/components/checkout/OrderDetail.vue
@@ -1,15 +1,85 @@
 <template>
-  <div>
-    
+  <div class="order-detail align-left">
+    <h3>
+      Your Order
+      <span class="edit-bag">Edit Bag</span>
+    </h3>
+    <hr>
+    <ul class="orders">
+      <li v-for="(item, itemIndex) in orders" v-bind:key="itemIndex">
+        <b-row>
+          <b-col md="3">
+            <div
+              class="order-img"
+              v-if="item.picture_urls.length > 0"
+              :style="orderPicture(item.picture_urls[0])"
+            ></div>
+          </b-col>
+          <b-col md="5" class="hello">
+            {{item.name}}
+            <br>
+            <span style="font-size: 12px">Standard Shipping</span>
+          </b-col>
+          <b-col>{{item.quantity}}</b-col>
+          <b-col>$ {{item.price}}</b-col>
+        </b-row>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
+  name: "OrderDetail",
+  data() {
+    return {};
+  },
 
-}
+  methods: {
+    orderPicture(img) {
+      return {
+        "background-image": "url(" + img + ")",
+        width: "100%",
+        height: "70px",
+        "background-size": "contain",
+        'background-repeat': 'no-repeat'
+
+      };
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      orders: "cartStore/getCart"
+    })
+  }
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.edit-bag {
+  font-size: 18px;
+  margin-left: 20px;
+  color: #bdbdbd;
+  cursor: pointer;
+}
 
+.order-detail {
+  .orders {
+    list-style-type: none;
+    padding: 0px;
+    width: 100%;
+
+    li {
+      padding: 10px;
+      margin-bottom: 10px;
+      width: 100%;
+
+      &:nth-child(even) {
+        background: #eeeeee;
+      }
+    }
+  }
+}
 </style>
--- src/store/cart.js
+++ src/store/cart.js
@@ -31,5 +31,9 @@ export default {
       }
     },
   },
-  getters: {},
+  getters: {
+    getCart(state){
+      return state.cart;
+    }
+  },
 };
