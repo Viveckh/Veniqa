59e453a2ba32864da2031bacee3ed2dbff112ce5
Sujil Maharjan <smaharj1@ramapo.edu>
2018-12-04 23:18:08 -0500

    Editing the order added

--- src/components/checkout/OrderDetail.vue
+++ src/components/checkout/OrderDetail.vue
@@ -2,12 +2,25 @@
   <div class="order-detail align-left">
     <h3>
       Your Order
-      <span class="edit-bag">Edit Bag</span>
+      <span
+        class="edit-bag"
+        @click="editOrder()"
+        v-if="!editMode && orders.length > 0"
+      >Edit Bag</span>
+      <span class="edit-bag delete" v-if="editMode" @click="deleteSelected()">Delete</span>
+      <span class="edit-bag" v-if="editMode" @click="done()">Done</span>
     </h3>
     <hr>
     <ul class="orders">
       <li v-for="(item, itemIndex) in orders" v-bind:key="itemIndex">
         <b-row>
+          <b-col sm="1" v-if="editMode">
+            <b-form-checkbox
+              class="checkbox-item"
+              :id="'order-'+itemIndex"
+              @change.native="itemClicked(item)"
+            ></b-form-checkbox>
+          </b-col>
           <b-col md="3">
             <div
               class="order-img"
@@ -15,12 +28,23 @@
               :style="orderPicture(item.picture_urls[0])"
             ></div>
           </b-col>
-          <b-col md="5" class="hello">
+          <b-col md="4">
             {{item.name}}
             <br>
             <span style="font-size: 12px">Standard Shipping</span>
           </b-col>
-          <b-col>{{item.quantity}}</b-col>
+          <b-col>
+            <b-form-input
+              v-model="item.quantity"
+              v-if="editMode"
+              :state="quantityState(item.quantity)"
+              type="number"
+              min="1"
+              aria-describedby="qtyFeedback"
+            ></b-form-input>
+            <b-form-invalid-feedback v-if="editMode" id="qtyFeedback"> > 0 </b-form-invalid-feedback>
+            <span v-else>{{item.quantity}}</span>
+          </b-col>
           <b-col>$ {{item.price}}</b-col>
         </b-row>
       </li>
@@ -29,41 +53,91 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'OrderDetail',
+  name: "OrderDetail",
   data() {
-    return {};
+    return {
+      editMode: false,
+      selectedItems: []
+    };
   },
 
   methods: {
     orderPicture(img) {
       return {
-        'background-image': `url(${img})`,
-        width: '100%',
-        height: '70px',
-        'background-size': 'contain',
-        'background-repeat': 'no-repeat',
-
+        "background-image": `url(${img})`,
+        width: "100%",
+        height: "70px",
+        "background-size": "contain",
+        "background-repeat": "no-repeat"
       };
     },
+
+    editOrder() {
+      this.editMode = true;
+    },
+
+    itemClicked(item) {
+      // Replace with product ID.
+      let foundIndex = _.findIndex(this.selectedItems, item);
+
+      if (foundIndex < 0) {
+        this.selectedItems.push(item);
+      } else {
+        this.selectedItems.splice(foundIndex, 1);
+      }
+    },
+
+    done() {
+      let validated = true;
+      for(let i =0; i < this.orders.length ; i++) {
+        let item = this.orders[i];
+        if(!this.quantityState(item.quantity)) {
+          validated = false;
+        }
+      }
+
+      if(validated){
+        this.editMode = false;
+      this.selectedItems = [];
+      }
+      
+    },
+
+    deleteSelected() {
+      this.$store.commit("cartStore/deleteOrders", this.selectedItems);
+      this.selectedItems = [];
+    }
   },
 
   computed: {
     ...mapGetters({
-      orders: 'cartStore/getCart',
+      orders: "cartStore/getCart"
     }),
-  },
+
+    quantityState() {
+      return qty => {
+        return qty >= 1;
+      };
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/css/global.scss";
+
 .edit-bag {
   font-size: 18px;
   margin-left: 20px;
   color: #bdbdbd;
   cursor: pointer;
+
+  &.delete {
+    color: $primary-red;
+  }
 }
 
 .order-detail {
@@ -71,6 +145,10 @@ export default {
     list-style-type: none;
     padding: 0px;
     width: 100%;
+
+    .checkbox-item {
+      padding-top: 20px;
+    }
 
     li {
       padding: 10px;
