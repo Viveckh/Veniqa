c9787efe88db55bc2164d4e0533a6cb7bb783f6f
Sujil Maharjan <smaharj1@ramapo.edu>
2018-12-12 18:33:23 -0500

    url done

--- src/components/homepage/ManagePhoto.vue
+++ src/components/homepage/ManagePhoto.vue
@@ -2,6 +2,16 @@
   <div id="manage-photo">
     <div>
       <b-form-file :multiple="true" v-model="imageInput" @input="fileUploaded()" class="mt-3"></b-form-file>
+      <p class="align-center">OR</p>
+      <b-form-group
+        id="remoteUrl"
+        description="Paste a remote URL."
+        label="Remote Url"
+        label-for="remote-url"
+        @
+      >
+        <b-form-input id="remote-url" v-model.trim="remoteUrl" @keyup.enter.native="loadImage()"></b-form-input>
+      </b-form-group>
     </div>
 
     <div class="image-displayer">
@@ -64,12 +74,12 @@
 </template>
 
 <script>
-import ImageShowcase from '@/components/homepage/ImageShowcase';
+import ImageShowcase from "@/components/homepage/ImageShowcase";
 
 export default {
-  name: 'ManagePhoto',
+  name: "ManagePhoto",
   components: {
-    ImageShowcase,
+    ImageShowcase
   },
   data() {
     return {
@@ -87,46 +97,48 @@ export default {
       size: {
         standard: {
           height: 800,
-          width: 600,
+          width: 600
         },
         thumbnail: {
           height: 400,
-          width: 300,
-        },
+          width: 300
+        }
       },
+
+      remoteUrl: ''
     };
   },
 
   methods: {
     async cropImage() {
-      const largeUrl = await this.cropSection.promisedBlob('image/png');
+      const largeUrl = await this.cropSection.promisedBlob("image/png");
       const thumbnailUrl = await this.thumbnailCrop.promisedBlob(
-        'image/png',
-        0.25,
+        "image/png",
+        0.25
       );
 
       // Checks the index in final images list with the file name.
       const index = _.findIndex(
         this.finalImages,
-        file => file.name === this.largeInitImage.name,
+        file => file.name === this.largeInitImage.name
       );
       if (index >= 0) {
         this.finalImages[index].largeUrl = URL.createObjectURL(largeUrl);
         this.finalImages[index].thumbnailUrl = URL.createObjectURL(
-          thumbnailUrl,
+          thumbnailUrl
         );
       }
     },
     fileUploaded() {
       if (this.imageInput.length > 0) {
-        this.imageInput.forEach((file) => {
+        this.imageInput.forEach(file => {
           if (
             _.findIndex(this.finalImages, obj => obj.name === file.name) < 0
           ) {
             this.finalImages.push({
               name: file.name,
               thumbnailUrl: URL.createObjectURL(file),
-              largeUrl: URL.createObjectURL(file),
+              largeUrl: URL.createObjectURL(file)
             });
           }
         });
@@ -135,15 +147,28 @@ export default {
     imageChosenFromList(imageObj) {
       this.setImages(imageObj.largeUrl, imageObj.thumbnailUrl, imageObj.name);
 
-      this.cropSection.refresh();
-      this.thumbnailCrop.refresh();
+      // this.cropSection.refresh();
+      // this.thumbnailCrop.refresh();
     },
+    loadImage(){
+      console.log("Load image trigger");
 
+      let newObj = {
+        name: 'remoteUrl'+this.finalImages.length,
+        thumbnailUrl: this.remoteUrl,
+        largeUrl: this.remoteUrl
+      };
+
+      this.setImages(this.remoteUrl, this.remoteUrl, newObj.name);
+      this.finalImages.push(newObj);
+      this.remoteUrl = '';
+      
+    },
     fileChosenInLarge(file) {
       let newObj = {
         name: file.name,
         thumbnailUrl: URL.createObjectURL(file),
-        largeUrl: URL.createObjectURL(file),
+        largeUrl: URL.createObjectURL(file)
       };
 
       let ind = _.findIndex(this.finalImages, obj => obj.name === newObj.name);
@@ -151,7 +176,7 @@ export default {
         this.finalImages.push({
           name: file.name,
           thumbnailUrl: URL.createObjectURL(file),
-          largeUrl: URL.createObjectURL(file),
+          largeUrl: URL.createObjectURL(file)
         });
 
         ind = this.finalImages.length - 1;
@@ -160,33 +185,38 @@ export default {
       newObj = this.finalImages[ind];
 
       this.setImages(newObj.largeUrl, newObj.thumbnailUrl, newObj.name);
-      this.thumbnailCrop.refresh();
-      this.cropSection.refresh();
+      
     },
 
     imageRemoveTrigger() {
       this.setImages(null, null, null);
-      this.thumbnailCrop.refresh();
+      // this.thumbnailCrop.refresh();
     },
 
     setImages(url, thumbUrl, name) {
       this.thumbnailInitImage = {
         url: thumbUrl,
-        name,
+        name
       };
 
       this.largeInitImage = {
         url,
-        name,
+        name
       };
+
+      this.thumbnailCrop.refresh();
+      this.cropSection.refresh();
     },
 
     resetImagesModification() {
-      const index = _.findIndex(this.finalImages, file => file.name === this.largeInitImage.name);
+      const index = _.findIndex(
+        this.finalImages,
+        file => file.name === this.largeInitImage.name
+      );
       this.finalImages.splice(index, 1);
       this.cropSection.remove();
-    },
-  },
+    }
+  }
 };
 </script>
 
