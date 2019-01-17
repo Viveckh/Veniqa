1584e35dc2581beb2c809507ec2c9c79e35e645b
Vivek Pandey (EJ, Vek) <vpandey@ramapo.edu>
2019-01-17 18:51:18 -0500

    Production Practices Integration 1 - USEFUL COMMIT FOR REFERENCE

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("Inside home page", req.sessionID);
   res.render('index', { title: 'Veniqa' });
 });
 
