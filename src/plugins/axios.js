ca4760fc4b46ca4092ac76a69c9676b77feeb3bd
Sujil Maharjan <smaharj1@ramapo.edu>
2019-01-16 20:11:59 -0500

    HTTP Call interceptor and loader added for each http calls made.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,8 +1,31 @@
 import Vue from 'vue';
 import axios from 'axios';
+import { eventHub } from "@/utils/EventHub";
 
 const instance = axios.create({
   baseURL: 'https://veniqa.azurewebsites.net/',
   withCredentials: true,
 });
+
+instance.interceptors.request.use(
+  conf => {
+    console.log('eventhub', eventHub)
+      eventHub.$emit('before-request');
+      return conf;
+  },
+  error => {
+      eventHub.$emit('request-error');
+      return Promise.reject(error);
+  }
+);
+instance.interceptors.response.use(
+  response => {
+      eventHub.$emit('after-response');
+      return response;
+  },
+  error => {
+      eventHub.$emit('response-error');
+      return Promise.reject(error);
+  }
+);
 Vue.prototype.$axios = instance;
