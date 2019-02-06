9223799a415f90ad934e926078b2542a1ccf08d4
Sujil Maharjan <smaharj1@ramapo.edu>
2019-02-05 19:48:13 -0500

    Image Hover next image for search result

--- src/components/vendor-pages/SearchResultView.vue
+++ src/components/vendor-pages/SearchResultView.vue
@@ -4,17 +4,18 @@
       <h1>Search Results</h1>
       <div class="result-view">
         <div class="product-card" v-for="(product, pid) in data" v-bind:key="pid">
-          <div class="link" @click="openProductDetail(product._id)">
+          <div class="link " @click="openProductDetail(product._id)">
             <div class="img-parent" v-if="product.thumbnailUrls.length > 0">
-              <div class="img-cls" :style="getPictureStyle(product.thumbnailUrls[0])"></div>
+              <search-result-view-image :product="product"/>
             </div>
 
             <p v-else style="font-size: 5em; padding: 10px 0px; text-align: center; color: #bdbdbd">
               <font-awesome-icon icon="shopping-bag" width="100%"/>
             </p>
-            <div class="product-card-desc align-center">
-              <h5>{{product.name}}</h5>
-              <p>{{product.price.currency}} {{product.price.amount}}</p>
+            <div class="product-card-desc">
+              <p class="info">{{product.store}}</p>
+              <p class="title">{{product.name}}</p>
+              <p><strong>{{product.price.currency}} {{product.price.amount}}</strong></p>
             </div>
           </div>
           <!-- <b-button class="primary-button add-cart-button" @click="addToCart(product)">Add to Cart</b-button> -->
@@ -26,6 +27,7 @@
 
 <script>
 import notification from '@/services/NotificationService';
+import SearchResultViewImage from '@/components/vendor-pages/SearchResultViewImage';
 
 export default {
   name: 'SearchResultView',
@@ -34,6 +36,9 @@ export default {
       type: Array,
       required: true,
     },
+  },
+  components: {
+    SearchResultViewImage
   },
 
   methods: {
@@ -99,27 +104,30 @@ export default {
   // box-shadow: 3px 4px 5px 0px #ccc;
   // background-color: white;
   border-radius: 0.25rem;
-  margin: 20px 20px 20px 0px;
-  width: 300px;
+  margin: 20px 20px 30px 0px;
+  width: 270px;
 
-  &:hover .img-cls {
-    transform: scale(1.2);
-    transition: all 0.5s;
-  }
+  // &:hover .img-cls {
+  //   transform: scale(1.2);
+  //   transition: all 0.5s;
+  // }
 
   .img-parent {
-    height: 350px;
-    width: 300px;
+    height: 315px;
+    width: 270px;
     overflow: hidden;
   }
 
   .product-card-desc {
-    margin-top: 0.75em;
+    margin-top: 0.5em;
     .title {
-      height: 3.5em;
+      height: 2em;
       text-overflow: ellipsis;
       overflow: auto;
     }
+
+    
+    
 
     .price {
       font-weight: bold;
@@ -130,16 +138,16 @@ export default {
     cursor: pointer;
   }
   p {
-    padding: 5px 5px;
+    padding: 3px 5px;
     margin: 0px;
   }
 
-  .img-cls {
-    border-top-right-radius: 0.25rem;
-    border-top-left-radius: 0.25rem;
-    height: 100%;
-    width: 100%;
-  }
+  // .img-cls {
+  //   border-top-right-radius: 0.25rem;
+  //   border-top-left-radius: 0.25rem;
+  //   height: 100%;
+  //   width: 100%;
+  // }
 
   .add-cart-button {
     width: 100%;
---
+++ src/components/vendor-pages/SearchResultViewImage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="search-result-view-image">
+    <!-- <div class="img-cls" @mouseover="isHover = true" @mouseleave="isHover = false" :style="getPictureStyle()"></div> -->
+    <img v-show="!isHover" class="img-cls" @mouseover="isHover = true" :src="product.thumbnailUrls[0]"/>
+    <img v-show="isHover" class="img-cls"  @mouseleave="isHover = false" :src="product.thumbnailUrls.length > 1 ? product.thumbnailUrls[1] : product.thumbnailUrls[0]" >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResultViewImage',
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    }
+  },
+
+  data() {
+    return {
+      isHover: false,
+    }
+  },
+
+  methods: {
+    getPictureStyle() {
+      let picture = this.product.thumbnailUrls[0];
+
+      if(this.isHover && this.product.thumbnailUrls.length > 1) {
+        picture = this.product.thumbnailUrls[1];
+      }
+      return {
+        'background-image': `url(${picture})`,
+        'background-size': 'cover',
+        // width: 'auto',
+        // height: '350px',
+        'margin-bottom': '10px',
+      };
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+#search-result-view-image {
+  height: 100%;
+  width: 100%;
+}
+.img-cls {
+    border-top-right-radius: 0.25rem;
+    border-top-left-radius: 0.25rem;
+    height: auto;
+    width: 100%;
+  }
+</style>
+
