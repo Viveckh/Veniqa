b04866155bf0c811abd17e4b5c8a31e4f915b281
Vivek Pandey (EJ, Vek) <vpandey@ramapo.edu>
2019-02-06 20:12:19 -0500

    sujil cha

--- app.js
+++ app.js
@@ -125,9 +125,11 @@ app.use(passport.session());
 // To Allow cross origin requests originating from selected origins
 app.use(function(req, res, next) {
   var origin = req.headers.origin;
-  //if(ALLOWED_ORIGINS.indexOf(origin) > -1){
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  //}
+  /*
+  if(ALLOWED_ORIGINS.indexOf(origin) > -1){
+      res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  */
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
