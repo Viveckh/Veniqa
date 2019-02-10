6c705aa63964ca54f1209d8b2f3046c2c1493f8e
Vivek Pandey (EJ, Vek) <vpandey@ramapo.edu>
2019-02-09 19:37:06 -0500

    using home folder for config in prod

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fs from 'fs';
 if (process.env.NODE_ENV == 'production') {
     // Load the .env file from the system location where it is stored
     // dotenv.config({path: '/var/veniqa'}); // Since an override option is missing, using the hack below
-    const envConfig = dotenv.parse(fs.readFileSync('/etc/veniqa/client-server/production/.env'))
+    const envConfig = dotenv.parse(fs.readFileSync('/home/veniqa/client-server/production/.env'))
     for (let k in envConfig) {
         process.env[k] = envConfig[k]
     }
