12a4a5c3ea3d2f6f8c6fc18c19df554a4b591a99
Sujil Maharjan <smaharj1@ramapo.edu>
2019-07-19 21:06:34 -0400

    - Some unnecessary codes removed - Khalti key dumped into the config.json

--- shopping-webclient/src/components/checkout/khalti/KhaltiEntry.vue
+++ shopping-webclient/src/components/checkout/khalti/KhaltiEntry.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- < id="khalti-entry"> -->
-    <button id="khalti-button" @click="startCheckout()" :style="getClass">{{text}} </button>
+  <button id="khalti-button" @click="startCheckout()" :style="getClass">{{text}}</button>
   <!-- </div> -->
 </template>
 
 <script>
-import KhaltiCheckout from "khalti-web"; 
+import KhaltiCheckout from 'khalti-web';
 import _ from 'lodash';
 
 export default {
@@ -18,7 +18,7 @@ export default {
     text: {
       type: String,
       required: false,
-      default: 'Khalti Pay'
+      default: 'Khalti Pay',
     },
 
     cls: {
@@ -29,78 +29,83 @@ export default {
     total: {
       type: Number,
       required: true,
-    }
+    },
   },
 
   data() {
     return {
       mainClass: null,
-    }
+    };
   },
 
   created() {
     this.mainClass = {
-      'padding': '0.5rem 2rem',
-      'background': '#773292',
-      'color': 'white',
-      'border': 'none',
-      'margin-top': '2rem'
+      padding: '0.5rem 2rem',
+      background: '#773292',
+      color: 'white',
+      border: 'none',
+      'margin-top': '2rem',
     };
 
-    if(!this.cls){
+    if (!this.cls) {
       _.assignIn(this.mainClass, this.cls);
     }
 
     this.configureKhalti();
   },
 
-  methods:{
+  methods: {
     startCheckout() {
-      this._checkout.show({amount: this.total});
+      this._checkout.show({ amount: this.total });
     },
 
     configureKhalti() {
-    // Create Khalti Instance before mount.
-    let that = this;
-    let khaltiConfig = {
-      // replace this key with yours
-      "publicKey": "test_public_key_dc74e0fd57cb46cd93832aee0a390234",
-      "productIdentity": this.config.productIdentity ? this.config.productIdentity : "1234567890",
-      "productName": this.config.productName ? this.config.productName : "Drogon",
-      "productUrl": this.config.productUrl ? this.config.productUrl : 'https://www.veniqa.com',
-      "eventHandler": {
-            onSuccess (payload) {
-                // hit merchant api for initiating verfication
-                that.$emit('success',payload);
-            },
-            // onError handler is optional
-            onError (error) {
-                // handle errors
-                that.$emit('error', error);
-            },
-            onClose () {
-              that.$emit('close');
+      // Create Khalti Instance before mount.
+      const that = this;
 
-              // console.log('widget is closing');
-            }
-        }
+      const khaltiConfig = {
+        // replace this key with yours
+        publicKey: this.config.key,
+        productIdentity: this.config.productIdentity
+          ? this.config.productIdentity
+          : '1234567890',
+        productName: this.config.productName
+          ? this.config.productName
+          : 'Drogon',
+        productUrl: this.config.productUrl
+          ? this.config.productUrl
+          : 'https://www.veniqa.com',
+        eventHandler: {
+          onSuccess(payload) {
+            // hit merchant api for initiating verfication
+            that.$emit('success', payload);
+          },
+          // onError handler is optional
+          onError(error) {
+            // handle errors
+            that.$emit('error', error);
+          },
+          onClose() {
+            that.$emit('close');
+
+            // console.log('widget is closing');
+          },
+        },
       };
 
       this._checkout = new KhaltiCheckout(khaltiConfig);
     },
-    
   },
 
   computed: {
     getClass() {
       return this.mainClass;
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
 #khalti-entry {
 }
 </style>
-
