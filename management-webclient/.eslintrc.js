b1028baf9c49769322ad8574a83a632ef8650575
Sujil Maharjan <smaharj1@ramapo.edu>
2020-02-19 17:30:35 -0500

    Switching off the no-console policy

--- management-webclient/.eslintrc.js
+++ management-webclient/.eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/airbnb'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': 'off',
     'no-param-reassign': 'off',
