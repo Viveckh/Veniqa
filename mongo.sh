37086950e7ef3fa4044a27dab3f23018acf11a4e
Sujil Maharjan <smaharj1@ramapo.edu>
2020-03-01 20:59:47 -0500

    Docker setup with local Mongo Instance (#86)
    * Local docker setup for both UIs.
    
    * Using dockers for local for node servers with bind mounts
    
    * Local docker setup with bind mount
    
    * Changing UI envs for now since docker-compose for UI envs is a bit different
    
    * Mongo pointed to docker for development

---
+++ mongo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Initialize a mongo data folder and logfile
+mkdir -p /data/db
+touch /var/log/mongodb.log
+chmod 777 /var/log/mongodb.log
+
+# Start mongodb with logging
+# --logpath    Without this mongod will output all log information to the standard output.
+# --logappend  Ensure mongod appends new entries to the end of the logfile. We create it first so that the below tail always finds something
+docker-entrypoint.sh mongod --logpath /var/log/mongodb.log --logappend &
+
+# Wait until mongo logs that it's ready (or timeout after 60s)
+COUNTER=0
+grep -q 'waiting for connections on port' /var/log/mongodb.log
+while [[ $? -ne 0 && $COUNTER -lt 60 ]] ; do
+    sleep 2
+    let COUNTER+=2
+    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
+    grep -q 'waiting for connections on port' /var/log/mongodb.log
+done
+
+# Restore from dump
+mongorestore /home/dump
+
+# Keep container running
+tail -f /dev/null
