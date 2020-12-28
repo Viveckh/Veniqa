1a5154be462199009ff7fead0b82d714e1904bf4
gfyre <drumpatel@gmail.com>
2020-12-28 11:10:30 +0530

    Handle and show errors while making payment

--- shopping-webclient/src/components/checkout/stripe/Stripe.vue
+++ shopping-webclient/src/components/checkout/stripe/Stripe.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <!-- <stripe-payment/> -->
-    <card class='stripe-card'
-      :class='{ complete }'
-      :stripe='stripeKey'
-      :options='stripeOptions'
-      :paymentReqOptions='reqParams'
-      @change='complete = $event.complete'
-      @token='payWithGpay'
+    <card
+      class="stripe-card"
+      :class="{ complete }"
+      :stripe="stripeKey"
+      :options="stripeOptions"
+      :paymentReqOptions="reqParams"
+      @change="complete = $event.complete"
+      @token="payWithGpay"
     />
-    <br>
-    <b-btn class='pay-with-stripe' @click='pay' :disabled='!complete'>Pay with credit card</b-btn>
+    <br />
+    <b-btn
+      class="pay-with-stripe"
+      @click="pay($event)"
+      :disabled="!complete"
+      style="font-family: Libre Baskerville; font-size: 14px"
+      >Pay by card</b-btn
+    >
   </div>
 </template>
 
@@ -55,7 +62,7 @@ export default {
   },
 
   methods: {
-    async pay() {
+    async pay(event) {
       // createToken returns a Promise which resolves in a result object with
       // either a token or an error key.
       // See https://stripe.com/docs/api#tokens for the token object.
@@ -64,31 +71,46 @@ export default {
       try {
         // const data = await Stripe.createToken();
         // this.$emit('pay', data.token);
+        event.target.disabled = true;
         let checkoutId = this.checkoutId;
         const { data } = await Vue.prototype.$axios({
           url: ProxyUrl.stripeInstantPay,
           method: 'post',
           data: {
-            checkoutId
+            checkoutId,
           },
         });
-      if (data && data.httpStatus === 200) {
-        console.log(data.responseData);
-        if (data.responseData) {
-          const paymentMethodReq =  await Stripe.createPaymentMethod('card', {});
-          console.log(paymentMethodReq);
-  
-          if(paymentMethodReq.paymentMethod.id){
-              const confirmPayment = await Stripe.confirmCardPayment(data.responseData.client_secret, {
-              payment_method: paymentMethodReq.paymentMethod.id
-              });
-  
+        if (data && data.httpStatus === 200) {
+          console.log(data.responseData);
+          if (data.responseData) {
+            const paymentMethodReq = await Stripe.createPaymentMethod(
+              'card',
+              {}
+            );
+            console.log(paymentMethodReq);
+
+            if (paymentMethodReq.paymentMethod.id) {
+              const confirmPayment = await Stripe.confirmCardPayment(
+                data.responseData.client_secret,
+                {
+                  payment_method: paymentMethodReq.paymentMethod.id,
+                }
+              );
+
               console.log(confirmPayment);
-          
-              if(confirmPayment){
+
+              if (confirmPayment) {
                 console.log('inside confirmPayment', confirmPayment);
-                if(confirmPayment.paymentIntent.status == 'succeeded'){
+                if (confirmPayment.paymentIntent.status == 'succeeded') {
                   console.log('inside confirmPayment status', confirmPayment);
+                  if (confirmPayment.hasOwnProperty('error')) {
+                    this.$notify({
+                      group: 'toast',
+                      type: 'error',
+                      text: `${confirmPayment.error.code},  ${confirmPayment.error.message}. Please try again later`,
+                    });
+                    event.target.disabled = false;
+                  }
                   const { data } = await Vue.prototype.$axios({
                     url: ProxyUrl.stripeInstantPayment,
                     method: 'post',
@@ -101,34 +123,31 @@ export default {
                     return this.$router.push('/orders');
                     //return data.responseData;
                   }
-                    //this.createPayment(confirmPayment.paymentIntent.id);
-                }else{
+                  //this.createPayment(confirmPayment.paymentIntent.id);
+                } else {
+                  event.target.disabled = false;
                   throw new Error('Card  details are invalid !');
                 }
-                  
-  
               }
-  
+            }
           }
-  
+          // const { data } = await Vue.prototype.$axios({
+          //   url: ProxyUrl.stripePay,
+          //   method: 'post',
+          //   data: {
+          //     checkoutId,
+          //     paymentToken: token.id,
+          //   },
+          // });
+          /**
+           * Response data:
+           * {
+           *    orderId: 'xxx'
+           * }
+           */
+          //return data.responseData;
         }
-        // const { data } = await Vue.prototype.$axios({
-        //   url: ProxyUrl.stripePay,
-        //   method: 'post',
-        //   data: {
-        //     checkoutId,
-        //     paymentToken: token.id,
-        //   },
-        // });
-        /**
-         * Response data:
-         * {
-         *    orderId: 'xxx'
-         * }
-        */
-        //return data.responseData;
-      }
-      //  const data = await Stripe.createToken();
+        //  const data = await Stripe.createToken();
         console.log('stripe payWithStripe token', this.checkoutId);
         //this.$emit('pay', data.token);
       } catch (error) {
@@ -152,13 +171,11 @@ export default {
     //   return params;
     // }
   },
-
-
 };
 </script>
 
 <style lang="scss" scoped>
-.pay-with-stripe{
+.pay-with-stripe {
   padding: 5px;
   background-image: linear-gradient(to right, #267871, #136a8a) !important;
 }
